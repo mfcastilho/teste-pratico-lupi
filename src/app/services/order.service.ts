b14e4e310e003cfd98f2e84f84bb9cbbd94f6d82
mfcastilho <fredericocastilho@hotmail.com>
2024-12-07 16:29:08 -0300

    feat: create order service

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Order } from '../models/order.model';
+import { map } from 'rxjs/operators';
+import { Timestamp } from 'firebase/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  constructor(private firestore: AngularFirestore) {}
+
+getOrders() {
+  return this.firestore.collection('orders').snapshotChanges().pipe(
+    map(actions => actions.map(a => {
+      const data = a.payload.doc.data() as Order;
+      const id = a.payload.doc.id;
+
+      if (data.createdAt instanceof Timestamp) {
+        data.createdAt = data.createdAt.toDate();
+      }
+
+      return { id, ...data };
+    }))
+  );
+}
+
+  addOrder(order: Order) {
+    const newOrder = {
+      ...order,
+      createdAt: new Date()
+    };
+    return this.firestore.collection('orders').add(newOrder);
+  }
+
+  updateOrder(orderId: string, updatedOrder: Order) {
+    return this.firestore.collection('orders').doc(orderId).update(updatedOrder);
+  }
+}
