9c2a135ff3653e7c8a58be8478ba13433239c207
mfcastilho <fredericocastilho@hotmail.com>
2024-12-07 15:39:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pedido-gerenciamento',
+  webDir: 'www'
+};
+
+export default config;
